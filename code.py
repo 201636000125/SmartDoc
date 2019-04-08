668adb4d6bc8d4b08888d3ae91504397d6a166b2
201636000125 <2445752589@qq.com>
2019-04-08 20:37:01 +0800

    生成html

--- code.py
+++ code.py
@@ -9,20 +9,24 @@ Name :SmartDoc
 
 
 #读取SRS.txt文件
-with open("SRS.txt", "r")as f:
-    message = f.read()
+file_srsTxt=open("SRS.txt", "r");
+file_srsTxt_message=file_srsTxt.read()
+file_srsTxt.close()
 
 #生成SRS.html文件
-with open("SRS.html", "w")as f:
-    f.write(message)
+file_srsHtml=open("SRS.html","w")
+file_srsHtml.write(file_srsTxt_message)
+print(file_srsTxt_message)
+file_srsHtml.close()
 
-
-#读取code.py文件
-with open("code.py", "r")as f:
-    message = f.read()
-
+#读取Scode.py文件
+file_codePy=open("code.py", "r",encoding="utf-8")
+file_codePy_message=file_codePy.read()
+file_codePy.close()
 
 #生成code.html文件
-with open("code.html", "w")as f:
-    f.write(message)
+file_codeHtml=open("code.html","w")
+file_codeHtml.write(file_codePy_message)
+print(file_codePy_message)
+file_srsHtml.close()
 
